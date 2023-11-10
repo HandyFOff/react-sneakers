4404a11fd8bdc4b0624a687f09294d8ffaad7a56
HandyF <86957456+HandyFOff@users.noreply.github.com>
2023-11-10 23:07:38 +0300

    small fix 0.0.8

--- src/components/Drawer/SuccsesfulDrawer/index.jsx
+++ src/components/Drawer/SuccsesfulDrawer/index.jsx
@@ -19,7 +19,7 @@ const SuccsesfulDrawer = ({setStatus}) => {
             </div>
             <div className={styles.info}>
                 <h1 className={styles.title}>Заказ оформлен!</h1>
-                <p className={styles.description}>Ваш заказ #{Number(orders[orders.length - 1].id) + 1} скоро будет передан курьерской доставке</p>
+                <p className={styles.description}>Ваш заказ #{orders.length < 1 ? 1 : Number(orders[orders.length - 1].id) + 1} скоро будет передан курьерской доставке</p>
             </div>
             <Button title={'Вернуться назад'} type={'back'} event={handlerDrawer}/>
         </div>
